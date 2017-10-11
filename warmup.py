ec726ce0d86aed9b9ace743a6e9b226f5501379f
Joseph Nofech <jnofech@ualberta.ca>
2017-10-11 14:05:33 -0600

    Simple exercise for getting back into the groove of things. It's good to be back!

---
+++ warmup.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+import astropy.io.fits as fits
+import astropy.units as u
+from spectral_cube import SpectralCube
+
+# Import data from .fits files.
+I_mom0 = fits.getdata('ngc1672_co21_12m+7m+tp_mom0.fits')
+I_max = fits.getdata('ngc1672_co21_12m+7m+tp_tpeak.fits')
+
+
+# Calculate line width, sigma.
+sigma = I_mom0 / (np.sqrt(2*np.pi * I_max))
+
+
+# Plotting sigma versus the mom0 array.
+plt.legend(loc='lower right')
+plt.plot(np.ravel(I_mom0), np.ravel(sigma), 'k.')
+plt.xlabel('$I_{mom0}$')
+plt.ylabel('$\sigma$')
+plt.title('Line Width vs mom0 Plot')
+
