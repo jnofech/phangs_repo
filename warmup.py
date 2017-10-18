58c2ee563baf62cc43114fac1532246198524c83
Joseph Nofech <jnofech@ualberta.ca>
2017-10-18 12:54:53 -0600

    Update: Now plots sigma vs radius.

--- warmup.py
+++ warmup.py
@@ -5,6 +5,10 @@ import math
 import astropy.io.fits as fits
 import astropy.units as u
 from spectral_cube import SpectralCube
+%matplotlib inline
+
+from galaxies import Galaxy
+
 
 # Import data from .fits files.
 I_mom0 = fits.getdata('ngc1672_co21_12m+7m+tp_mom0.fits')
@@ -25,4 +29,21 @@ plt.xscale('log')
 plt.yscale('log')
 
 plt.savefig('warmup_linewidth_vs_mom0.png')
+plt.clf()
 
+
+# Importing radius of each data point.
+gal = Galaxy('NGC1672')
+hdr = fits.getheader('ngc1672_co21_12m+7m+tp_mom0.fits')
+rad = gal.radius(header=hdr)
+
+
+# Plotting sigma vs radius.
+plt.legend(loc='lower right')
+plt.semilogy(np.ravel(rad), np.ravel(sigma), 'k.')
+plt.xlabel('$Radius$ $(Mpc)$')
+plt.ylabel('$\sigma$')
+plt.title('Line Width vs Radius')
+
+plt.savefig('warmup_linewidth_vs_radius.png')
+plt.clf()
