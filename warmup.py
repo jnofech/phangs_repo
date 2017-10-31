81cdd39defc6d1be046a07d76cee66d27ca66e80
Joseph Nofech <jnofech@ualberta.ca>
2017-10-31 17:05:58 -0600

    Update: Now includes calculation involving beam width.

--- warmup.py
+++ warmup.py
@@ -24,17 +24,20 @@ rad = gal.radius(header=hdr)
 rad = (rad * u.Mpc.to(u.kpc)) * u.kpc / u.Mpc           # Converts rad from Mpc to kpc.
 # Calculating width of each pixel, in parsecs.
 pixsizes_deg = wcs.utils.proj_plane_pixel_scales(wcs.WCS(hdr))     # The size of each pixel, in degrees. Ignore that third dimension; that's pixel size for the speed.
-pixsizes = pixsizes_deg[0] * np.pi / 180.
-pcperpixel =  pixsizes*d      # Number of parsecs per pixel.
+pixsizes = pixsizes_deg[0] * np.pi / 180.                          # Pixel size, in radians.
+pcperpixel =  pixsizes*d      					   # Number of parsecs per pixel.
+pcperdeg = pcperpixel / pixsizes_deg[0]                            # Number of parsecs per degree.
 
-
+# Getting beam width, in degrees and parsecs.
+beam = hdr['BMAJ']                                      # Beam size, in degrees
+beam = beam * pcperdeg                                  # Beam size, in pc
 
 # Import data from .fits files.
 I_mom0 = fits.getdata('ngc1672_co21_12m+7m+tp_mom0.fits')
-I_mom1 = fits.getdata('ngc1672_co21_12m+7m+tp_mom1.fits')      # Intensity-weighted mean velocity of data.
+I_mom1 = fits.getdata('ngc1672_co21_12m+7m+tp_mom1.fits')      # Intensity-weighted mean velocity of data, in km/s/pixel.
 I_max = fits.getdata('ngc1672_co21_12m+7m+tp_tpeak.fits')
-
-sobel = ndimage.sobel(I_mom1/pcperpixel)        # Sobel image gradient of I_mom1. Highlights edges.
+gradv = ndimage.sobel(I_mom1/pcperpixel)              # Sobel gradient of I_mom1; = grad(mean velocity), in km/s/pc
+gradvl = ndimage.sobel(I_mom1 * beam / pcperpixel)    # Sobel gradient of I_mom1 * beam width, in km/s
 
 
 
@@ -50,16 +53,16 @@ plt.yscale('log')
 plt.savefig('warmup_linewidth_vs_mom0.png')
 plt.clf()
 
-# Plotting sigma vs a Sobel image gradient of the intensity-weighted velocity (mom1).
+# Plotting sigma vs a Sobel image gradient of the intensity-weighted velocity (mom1) times beam width.
 plt.legend(loc='lower right')
-plt.plot(np.ravel(sobel), np.ravel(sigma), 'k.')
-plt.xlabel('Sobel Gradient of Intensity-Weighted Mean Velocity (km/s/pc)')
+plt.plot(np.ravel(gradvl), np.ravel(sigma), 'k.')
+plt.xlabel('grad($v_c*l$) (km/s)')
 plt.ylabel('$\sigma$')
-plt.title('Line Width vs Sobel Gradient of Intensity-Weighted Mean Velocity')
+plt.title('Line Width vs Sobel Gradient of (Mean Velocity * Beam Width)')
 plt.xscale('log')
 plt.yscale('log')
 
-plt.savefig('warmup_linewidth_vs_mom1.png')
+plt.savefig('warmup_linewidth_vs_gradvl.png')
 plt.clf()
 
 # Plotting sigma vs radius.
@@ -75,35 +78,48 @@ plt.clf()
 
 
 
-# Create log arrays for sigma, Sigma, and radius.
-logsigma = np.log10(sigma)
-logSigma = np.log10(Sigma)
-logR = np.log10(rad.value)
+# Create log arrays for sigma, Sigma, sobel(mean velocity), and radius.
+logsigma = np.ravel(np.log10(sigma))
+logSigma = np.ravel(np.log10(Sigma))
+loggradv = np.ravel(np.log10(gradv))
+logR = np.ravel(np.log10(rad.value))
 
 # Deleting any values that are not finite
 index = np.arange(logsigma.size)
 a = np.isfinite(logsigma)==False      # A boolean array that is True where logsigma is nan or inf.
 b = np.isfinite(logSigma)==False
-c = np.isfinite(logR) == False        # A bool. array that is True where logsigma, logSigma, OR logR is nan or inf.
-logsigma_clean = np.delete(logsigma, index[a+b+c])
-logSigma_clean = np.delete(logSigma, index[a+b+c])
-logR_clean = np.delete(logR, index[a+b+c])
+c = np.isfinite(loggradv)==False
+d = np.isfinite(logR) == False        # A bool. array that is True where logsigma, logSigma, OR logR is nan or inf.
+logsigma_clean = np.delete(logsigma, index[a+b+c+d])
+logSigma_clean = np.delete(logSigma, index[a+b+c+d])
+loggradv_clean = np.delete(loggradv, index[a+b+c+d])
+logR_clean = np.delete(logR, index[a+b+c+d])
+R_clean = np.delete(rad, index[a+b+c+d])
 
 # Overwriting the main arrays with the cleaned versions
 logsigma = logsigma_clean
 logSigma = logSigma_clean
+loggradv = loggradv_clean
 logR = logR_clean
+R = R_clean.value
 
 
 
-# Creating and saving a dataframe of logsigma, logSigma, logR
-dataframe = pd.DataFrame(data=zip(np.ravel(logsigma), np.ravel(logSigma), np.ravel(logR)), columns = ["$log(\sigma)$", "$log(\Sigma)$", "$log(R)$"])
-dataframe.to_csv('sigma_Sigma_R.csv',index=False,header=False)
+# Creating and saving a dataframe of logsigma, logSigma, loggradv, logR
+dataframe = pd.DataFrame(data=zip(np.ravel(logsigma), np.ravel(logSigma), np.ravel(loggradv)), columns = ["$log(\sigma)$", "$log(\Sigma)$", "$log(gradv)$"])
+#dataframe = pd.DataFrame(data=zip(np.ravel(logsigma), np.ravel(logSigma), np.ravel(logR)), columns = ["$log(\sigma)$", "$log(\Sigma)$", "$log(R)$"])
+#dataframe = pd.DataFrame(data=zip(np.ravel(logsigma), np.ravel(logSigma), np.ravel(R)), columns = ["$log(\sigma)$", "$log(\Sigma)$", "$R$"])
+dataframe.to_csv('sigma_Sigma_gradv.csv',index=False,header=False)
+#dataframe.to_csv('sigma_Sigma_R.csv',index=False,header=False)
+
 # Reloading this same dataframe, just for kicks
-df = pd.read_csv('sigma_Sigma_R.csv', names = ['logsigma','logSigma','logR'])
+df = pd.read_csv('sigma_Sigma_gradv.csv', names = ['logsigma','logSigma','loggradv'])
+#df = pd.read_csv('sigma_Sigma_R.csv', names = ['logsigma','logSigma','logR'])
+#df = pd.read_csv('sigma_Sigma_R.csv', names = ['logsigma','logSigma','R'])
 
 
-# Fitting: logsigma ~ logSigma + logR
-mod = smf.ols(formula='logsigma ~ logSigma + logR', data=df)
+# Fitting: logsigma ~ logSigma + logR (or loggradv)
+mod = smf.ols(formula='logsigma ~ logSigma + loggradv', data=df)
+#mod = smf.ols(formula='logsigma ~ logSigma + R', data=df)
 res = mod.fit()
 print(res.summary())
